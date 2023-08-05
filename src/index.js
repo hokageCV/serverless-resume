46e91f3b20ea366fbb8588c0c792f2770d9894de
hokageCV <chaitanyavaru@gmail.com>
2023-08-05 18:08:24 +0530

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import resumeData from './resume';
 
+const corsHeaders = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'GET, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type',
+}
+
 export default {
 	async fetch(request, env, ctx) {
 		const formatedResume = JSON.stringify(resumeData, null, 2)
 		return new Response(formatedResume, {
 			headers: {
 				'Content-Type': 'application/json',
+				...corsHeaders,
 			}
 		})
 	},
