ad8a612b457b3ddad2e6ff78341ac2cd63378e22
hokageCV <chaitanyavaru@gmail.com>
2023-08-05 17:49:32 +0530

    feat: add resume

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import resumeData from './resume';
 
 export default {
 	async fetch(request, env, ctx) {
-		const formatedResume = JSON.stringify(resumeData, null, 4)
+		const formatedResume = JSON.stringify(resumeData, null, 2)
 		return new Response(formatedResume, {
 			headers: {
 				'Content-Type': 'application/json',
